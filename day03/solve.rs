70719e25c3fcce4aa65eaa17b265ef9a72a59680
Julia Evans <julia@jvns.ca>
2022-12-03 07:33:06 -0500

    make the code more boring

--- day03/solve.rs
+++ day03/solve.rs
@@ -1,4 +1,3 @@
-use std::collections::HashSet;
 use std::io::Read;
 
 fn char_to_num(c: char) -> u32 {
@@ -9,11 +8,22 @@ fn char_to_num(c: char) -> u32 {
     }
 }
 
-fn get_duplicates(first: &[u32], second: &[u32]) -> Vec<u32> {
-    let first_set: HashSet<u32> = first.iter().cloned().collect();
-    let second_set: HashSet<u32> = second.iter().cloned().collect();
+fn get_duplicates2(first: &[u8], second: &[u8]) -> u32 {
+    for i in 0..first.len() {
+        if second.contains(&first[i]) {
+            return char_to_num(first[i] as char);
+        }
+    }
+    panic!("No duplicates found");
+}
 
-    return first_set.intersection(&second_set).cloned().collect();
+fn get_duplicates3(first: &[u8], second: &[u8], third: &[u8]) -> u32 {
+    for i in 0..first.len() {
+        if second.contains(&first[i]) && third.contains(&first[i]) {
+            return char_to_num(first[i] as char);
+        }
+    }
+    panic!("No duplicates found");
 }
 
 fn part1() {
@@ -23,9 +33,10 @@ fn part1() {
     let sum = input
         .lines()
         .map(|line| {
-            let nums: Vec<u32> = line.chars().map(char_to_num).collect();
-            let dups = get_duplicates(&nums[0..nums.len() / 2], &nums[nums.len() / 2..]);
-            dups.iter().sum::<u32>()
+            let bytes = line.as_bytes();
+            let first = &bytes[..bytes.len() / 2];
+            let second = &bytes[bytes.len() / 2..];
+            get_duplicates2(first, second)
         })
         .sum::<u32>();
     println!("{}", sum);
@@ -35,19 +46,19 @@ fn part2() {
     let mut input = String::new();
     std::io::stdin().read_to_string(&mut input).unwrap();
 
-    let sum = input
-        .lines()
-        .collect::<Vec<&str>>()
-        .chunks(3)
-        .map(|group| {
-            let nums1: Vec<u32> = group[0].chars().map(char_to_num).collect();
-            let nums2: Vec<u32> = group[1].chars().map(char_to_num).collect();
-            let nums3: Vec<u32> = group[2].chars().map(char_to_num).collect();
-            let dups = get_duplicates(&nums1, &nums2);
-            let dups = get_duplicates(&dups, &nums3);
-            dups.iter().sum::<u32>()
-        })
-        .sum::<u32>();
+    let mut lines = input.lines();
+    let mut sum = 0;
+
+    loop {
+        let (first, second, third) = match (lines.next(), lines.next(), lines.next()) {
+            (Some(first), Some(second), Some(third)) => {
+                (first.as_bytes(), second.as_bytes(), third.as_bytes())
+            }
+            _ => break,
+        };
+        sum += get_duplicates3(first, second, third);
+    }
+
     println!("{}", sum);
 }
 
