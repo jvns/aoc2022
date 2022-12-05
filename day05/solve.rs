d03fe0077b20f3f002444041718f513574e57a9c
Julia Evans <julia@jvns.ca>
2022-12-05 09:01:01 -0500

    day 5 part 1

---
+++ day05/solve.rs
@@ -0,0 +1,83 @@
+use std::io::Read;
+/*
+ *
+    [D]
+[N] [C]
+[Z] [M] [P]
+ 1   2   3
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2
+*/
+
+type Move = (usize, usize, usize);
+type State = Vec<Vec<char>>;
+fn parse_input() -> (State, Vec<Move>) {
+    let mut input = String::new();
+    std::io::stdin().read_to_string(&mut input).unwrap();
+    // split on ("\n\n")
+    let mut parts = input.split("\n\n");
+    let (stacks, moves) = (parts.next().unwrap(), parts.next().unwrap());
+
+    let lines: Vec<Vec<char>> = stacks.lines().map(|line| line.chars().collect()).collect();
+    let length = lines[0].len();
+    // transpose lines
+    let state: Vec<Vec<char>> = (0..length)
+        .map(|i| lines.iter().map(|line| line[i]).collect())
+        .enumerate()
+        .filter(|(i, _)| i % 4 == 1)
+        .map(|(_, v)| v)
+        .map(|mut v: Vec<char>| {
+            v.pop();
+            v.retain(|&c| c != ' ');
+            v.reverse();
+            v
+        })
+        .collect();
+    let moves: Vec<Move> = moves
+        .lines()
+        .map(|line| {
+            let parts: Vec<&str> = line.split(" ").collect();
+            let num = parts[1].parse().unwrap();
+            let from = parts[3].parse().unwrap();
+            let to = parts[5].parse().unwrap();
+            (num, from, to)
+        })
+        .collect();
+    (state, moves)
+}
+
+fn move_one(from: usize, to: usize, state: &mut State) {
+    let item = state[from - 1].pop();
+    state[to - 1].push(item.unwrap());
+}
+
+fn part1() {
+    let (mut state, moves) = parse_input();
+
+    for (num, from, to) in moves {
+        for _ in 0..num {
+            move_one(from, to, &mut state);
+        }
+    }
+    // print out the last item in each stack, no newline
+    for stack in state {
+        print!("{}", stack.last().unwrap());
+    }
+    println!();
+}
+
+fn part2() {}
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    if args[1] == "1" {
+        part1();
+    } else if args[1] == "2" {
+        part2();
+    } else {
+        panic!("invalid argument");
+    }
+}
