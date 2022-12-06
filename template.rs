3bb4e4a74ee9a3e5fd949bf666c40b53a7bb9e71
Julia Evans <julia@jvns.ca>
2022-12-06 08:20:01 -0500

    update template

--- template.rs
+++ template.rs
@@ -1,17 +1,22 @@
 use std::io::Read;
-fn part1() {
+
+fn read_stdin() -> String {
     let mut input = String::new();
-    std::io::stdin().read_to_string(&mut input).unwrap();
+    _ = std::io::stdin().read_to_string(&mut input);
+    return input;
 }
 
-fn part2() {}
+fn part1(input: String) {}
+
+fn part2(input: String) {}
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
+    let input = read_stdin();
     if args[1] == "1" {
-        part1();
+        part1(input);
     } else if args[1] == "2" {
-        part2();
+        part2(input);
     } else {
         panic!("invalid argument");
     }
